e085a5404ce42f47a76d2223dba6c500b27274f5
jaehun221 <jhl2211768@gmail.com>
2025-06-02 22:36:01 +0900

    commit!

---
+++ javaLevel3/src/poly/Car0/K3Car.java
@@ -0,0 +1,16 @@
+package poly.Car0;
+
+public class K3Car {
+
+    public void startEngine() {
+        System.out.println("K3Car.startEngin");
+    }
+
+    public void offEngine() {
+        System.out.println("K3Car.offEngine");
+    }
+
+    public void pressAccelerater() {
+        System.out.println("K3Car.pressAccelerator");
+    }
+}
---
+++ javaLevel3/src/poly/Car0/Model3Car.java
@@ -0,0 +1,16 @@
+package poly.Car0;
+
+public class Model3Car {
+
+    public void startEngine() {
+        System.out.println("K3CModel3Carar.startEngin");
+    }
+
+    public void offEngine() {
+        System.out.println("Model3Car.offEngine");
+    }
+
+    public void pressAccelerater() {
+        System.out.println("Model3Car.pressAccelerator");
+    }
+}
