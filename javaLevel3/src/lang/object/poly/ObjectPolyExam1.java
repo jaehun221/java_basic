6c49ccf3bf99c02f8319006574c2905a31925beb
jaehun221 <jhl2211768@gmail.com>
2025-06-10 16:54:50 +0900

    commit!

---
+++ javaLevel3/src/lang/object/poly/ObjectPolyExam1.java
@@ -0,0 +1,24 @@
+package lang.object.poly;
+
+public class ObjectPolyExam1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+
+        action(dog);
+        action(car);
+    }
+
+    private static void action(Object obj) {
+        // obj.sound(); 컴파일 오류, Object에는 sound()가 없다.
+        // obj.move();  컴파일 오류, Object에는 move()가 없다.
+
+        // 객체에 맞는 다운캐스팅 필요
+        // jdk 16 이상에서 사용
+//        if (obj instanceof Dog dog) {
+//            dog.sound();
+//        } else if (obj instanceof Car car) {
+//            car.move();
+//        }
+    }
+}
